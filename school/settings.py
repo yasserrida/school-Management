68f4c1b26dccbfa370b6ba6bf9f738e593efd38c
yasserrida <ridayassir.yr@gmail.com>
2021-06-28 15:58:30 +0100

    add to gitignore

--- school/settings.py
+++ school/settings.py
@@ -4,9 +4,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-rc10+ncnlb9$=r_vaudx-ur2)qhr*0)e*chu%juia#rre3)k)4'
 
