90a9c0b9eaa926599390984f25352b59c2dcb4c5
yasserrida <ridayassir.yr@gmail.com>
2021-06-29 16:24:25 +0100

    add views and urls

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
+    path('login', views.login, name='login'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('users', views.users, name='users'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from .models import student
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+sidebar = [{'name': 'Dashboard', 'url': '/dashboard', 'icon': 'fa-tv'},
+           {'name': 'Etudiants', 'url': '/etudiants', 'icon': 'fa-users'},
+           {'name': 'Notes', 'url': '/notes', 'icon': 'fa-sticky-note'}]
 
 
 def index(request):
-    return render(request, 'index.html', {})
+    return render(request, 'index.html', {'APP_Name': env('APP_NAME')})
+
+
+def login(request):
+    return render(request, 'login.html', {'APP_Name': env('APP_NAME')})
+
+
+def dashboard(request):
+    return render(request, 'pages/dashboard.html', {'APP_Name': env('APP_NAME'), 'side_bar_items': sidebar})
+
+
+def users(request):
+    return render(request, 'pages/users.html', {'APP_Name': env('APP_NAME'), 'side_bar_items': sidebar})
